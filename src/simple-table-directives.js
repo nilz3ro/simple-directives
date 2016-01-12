f3e5cfd614d2aa92144d9eab6edd139b2c1d8f34
Austin Buddington <austin.j.buddington@gmail.com>
2016-01-12 09:28:02 -0500

    added thead

--- src/simple-table-directives.js
+++ src/simple-table-directives.js
@@ -1,15 +1,29 @@
 angular.module('simple-table-directives', [])
-  .directive('sTable', sTable);
+  .directive('sTable', sTable)
+  .directive('sTableHead', sTableHead);
 
   function sTable() {
     return {
       restrict: "E",
       scope: {}, 
+      replace: true,
       transclude: true,
       template: "<table ng-transclude></table>",
-      link: function(scope, element, attributes) {
-              console.log("link fired now", scope, element, attributes)
+      link: function(scope, element, attributes, controller, transclude) {
             }
     };
   }
+  
+  function sTableHead() {
+    return {
+      restrict: "E",
+      scope: {}, 
+      replace: true,
+      transclude: true,
+      template: "<thead ng-transclude></thead>",
+      link: function(scope, element, attributes, controller, transclude) {
+            }
+    };
+  }
+  
 
