822d7ce5e87f8841272e883f1da68ae30236cb10
Austin Buddington <austin.j.buddington@gmail.com>
2016-01-26 18:57:33 -0500

    saving changes

--- src/simple-table-directives.js
+++ src/simple-table-directives.js
@@ -138,6 +138,7 @@
         scope: false, 
         replace: true,
         transclude: true,
+        require: ['?sRow', '?sRowRepeat'],
         template: '<td class="s-cell" ng-transclude></td>',
         controller: function($scope, $element, $attrs, $transclude) {
           $scope.model = $scope.$parent.sRowModel || $scope.$parent.model;
