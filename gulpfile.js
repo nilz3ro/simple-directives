90cbffe18aadcac39ae97fd2c053932b5313323d
Austin Buddington <austin.j.buddington@gmail.com>
2016-11-09 11:26:51 -0500

    Better build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 'use strict'
 var gulp = require('gulp')
-var concat = require('gulp-concat')
+var concat = require('gulp-concat-util')
 
 gulp.task('default', ['build:js'])
 
 gulp.task('build:js', function () {
   gulp.src('./src/modules/*.js')
-    .pipe(concat('simple-directives.js'))
+    .pipe(concat('simple-directives.js', { sep: ';' }))
     .pipe(gulp.dest('./dist'))
 
   gulp.src('./LICENSE')
